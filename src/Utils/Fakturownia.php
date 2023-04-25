3046af430d06c2ee11a9fdbc70891bd368356741
Mateusz Qunabu <mateusz@qunabu.com>
2023-04-25 12:49:15 +0200

    feature/php82 (#27)
    * feature/php82
    
    * "php-http/discovery": true
    
    * SafeDirFix
    
    * mock api calls
    
    ---------
    
    Co-authored-by: Mateusz Wojczal <mateusz@wojczal.com>
    Co-authored-by: Tomasz Smolarek <tomasz.smolarek@escolasoft.com>

--- src/Utils/Fakturownia.php
+++ src/Utils/Fakturownia.php
@@ -2,17 +2,23 @@
 
 namespace EscolaLms\FakturowniaIntegration\Utils;
 
-class Fakturownia extends \Abb\Fakturownia\Fakturownia
+use Abb\Fakturownia\Fakturownia as FakturowaniaCore;
+use Abb\Fakturownia\RestClientInterface as RestClientInterfaceCore;
+use Abb\Fakturownia\FakturowniaRestClient as FakturowniaRestClientCore;
+
+class Fakturownia extends FakturowaniaCore
 {
     private string $host;
+
     private string $token;
+
     public $response;
 
-    public function __construct()
+    public function __construct(RestClientInterfaceCore $restClient = null)
     {
         $this->setHost();
         $this->setToken();
-        parent::__construct($this->token . '/' . $this->host);
+        parent::__construct($this->token . '/' . $this->host, $restClient ?: new FakturowniaRestClientCore());
     }
 
     private function setHost()
